736741c7fe477ba86570c3e9276b73fb3e484d0c
Astillius <106243978+Astillius@users.noreply.github.com>
2022-05-28 17:16:49 +0300

    Добавлена функция ввода информации для шифрования.

--- GENER.py
+++ GENER.py
@@ -1,17 +1,33 @@
 def Greetings():
-    print("═" * 45)
+    print("═" * 50)
     print("Алоха! Я - разум по шифрованию текстов.\nВ мой перечень услуг входит:")
     print("1 - Шифровка. \n2 - Расшифровка.")
     start = int(input("Выбери нужную тебе функцию: "))
     if start == 1:
         crypt()
-    else:
-        decrypt()
-
-while(True):
-    Greetings()
-    text = 0
 
 def crypt():
+    info = input("Введи текст сообщениия, которое необходимо защифровать: ")
+    print("═" * 5, "Введите ключ, с помощью которого будет шифроваться текст. Например: '1 3 0 2'", "═" * 5)
+    main_key = input("Ключ: ")
+    spt = main_key.split(" ")
+    key = []
+    for e in key:
+        key.append(int(e))
+    print("═" * 5, "Виды шифрования: \n1 - Символьное шифрование.\n2 - Шифрование группы.\n3 - Шифрование слов")
+    option = int(input("Выберите вид шифрования: "))
+    if option == 1:
+        letcrpt(key, info)
+    if option == 2:
+        grpcrpt(key, info)
+    if option == 3:
+        wordcrpt(key, info)
+def letcrpt(key, info):
 
-def decrypt():
+def grpcrpt(key, info):
+
+def wordcrpt(key, info):
+
+
+while True:
+    Greetings()
