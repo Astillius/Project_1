72ce7e2ded5e85ef97450fc397dc194ba5e2e981
Astillius <106243978+Astillius@users.noreply.github.com>
2022-05-29 13:07:46 +0300

    Добавлена возможность шифрования по символам.

--- GENER.py
+++ GENER.py
@@ -11,45 +11,60 @@ def Greetings():
 def crypt():
     info = input("Введи текст сообщениия, которое необходимо защифровать: ")
     print("═" * 5, "Введите ключ, с помощью которого будет шифроваться текст. Например: '1 3 0 2'", "═" * 5)
-    main_key = input("Ключ: ")
-    spt = main_key.split(" ")
-    key = []
-    for e in key:
-        key.append(int(e))
-    print("═" * 5, "Виды шифрования: \n1 - Символьное шифрование.\n2 - Шифрование группы.\n3 - Шифрование слов", "═" * 5)
+    main_spt = input("Ключ: ")
+    spt = main_spt.split(" ")
+    print("═" * 5, "Виды шифрования:", "═" * 5, "\n1 - Символьное шифрование.\n2 - Шифрование группы.\n3 - Шифрование слов")
     option = int(input("Выберите вид шифрования: "))
     if option == 1:
-        letcrpt(key, info)
+        letcrpt(spt, info)
     if option == 2:
-        grpcrpt(key, info)
+        grpcrpt(spt, info)
     if option == 3:
-        wordcrpt(key, info)
-def letcrpt(key, info):
-
-def grpcrpt(key, info):
-
-def wordcrpt(key, info):
+        wordcrpt(spt, info)
 
 def decrypt():
     info = input("Введи текст сообщениия, которое необходимо расшифровать: ")
-    main_key = input("Ключ, с помощью которого было зашифровано сообщение: ")
-    spt = main_key.split(" ")
-    key = []
-    for e in key:
-        key.append(int(e))
-    print("═" * 5, "Способ, с помощью которого было защифровано сообщение: \n1 - Символьное шифрование.\n2 - Шифрование группы.\n3 - Шифрование слов", "═" * 5)
+    main_spt = input("Ключ, с помощью которого было зашифровано сообщение: ")
+    spt = main_spt.split(" ")
+    print("═" * 5, "Способ, с помощью которого было защифровано сообщение:", "═" * 5 , "\n1 - Символьное шифрование.\n2 - Шифрование группы.\n3 - Шифрование слов")
     option = int(input("Способ шифрования: "))
     if option == 1:
-        decrypt_let(key, info)
+        decrypt_let(spt, info)
     if option == 2:
-        decrypt_grp(key, info)
+        decrypt_grp(spt, info)
     if option == 3:
-        decrypt_word(key, info)
-def decrypt_let(key, info):
+        decrypt_word(spt, info)
 
-def decrypt_grp(key, info):
+def driver_code(spt, info):
+    code = ''
+    structure = ''
+    size_of_key = len(spt)
+    numeration = len(info)
+    for i in range(0, numeration, size_of_key):
+        structure = [info[i + j] for j in range(size_of_key)]
+        for j in range(size_of_key):
+            code += str(structure[size_of_key - int(spt[j]) - 1])
+    return code
 
-def decrypt_word(key, info):
+
+def letcrpt(spt, info):
+    size_of_key = len(spt)
+    numeration = len(info)
+    if numeration % size_of_key != 0:
+        for i in range(size_of_key - (numeration % size_of_key)):
+            info += str("0")
+    print(driver_code(spt, info))
+
+
+#def grpcrpt(spt, info):
+
+#def wordcrpt(spt, info):
+
+#def decrypt_let(spt, info):
+
+#def decrypt_grp(spt, info):
+
+#def decrypt_word(spt, info):
 
 
 while True:
